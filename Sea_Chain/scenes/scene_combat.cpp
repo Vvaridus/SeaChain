573c3c236a3bcab5f34893fd9c6cf7f132f07312
Connor Johnston <40429124@live.napier.ac.uk>
2021-04-03 13:46:41 +0100

    Converted Logger system to a library

--- Sea_Chain/scenes/scene_combat.cpp
+++ Sea_Chain/scenes/scene_combat.cpp
@@ -3,36 +3,31 @@
 #include <iostream>
 #include <thread>
 #include "scene_combat.h"
-#include "../logger.h"
+#include <logger.h>
 
 using namespace std;
 using namespace sf;
 
 void Combat::Load() {
-    //Logger::sceneLoading(typeid(*this).name());
-
+    Logger::addEvent(Logger::EventType::Scene, Logger::Action::Loading, "");
     ls::loadLevelFile("resources/combat.txt", 54);
 
     auto ho = Engine::getWindowSize().y - (ls::getHeight() * 40.f);
     ls::setOffset(Vector2f(0, ho));
 
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    
-    //Logger::sceneLoading(typeid(*this).name());
 
+    Logger::addEvent(Logger::EventType::Scene, Logger::Action::Loaded, "");
     setLoaded(true);
 }
 
 void Combat::UnLoad() {
-    //Logger::sceneUnloading(typeid(*this).name());
-
+    Logger::addEvent(Logger::EventType::Scene, Logger::Action::Unloaded, "");
   ls::unload();
   Scene::UnLoad();
 }
 
 void Combat::Update(const double& dt) {
-    //Logger::sceneUpdating(typeid(*this).name(), dt);
-
   Scene::Update(dt);
 }
 
