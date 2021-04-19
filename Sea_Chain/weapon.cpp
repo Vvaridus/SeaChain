a829043f6588755d81ef46da560c719ef662484e
Connor Johnston <40429124@live.napier.ac.uk>
2021-04-19 21:51:20 +0100

    Update weapon.cpp

--- Sea_Chain/weapon.cpp
+++ Sea_Chain/weapon.cpp
@@ -1,23 +1,23 @@
 #include "weapon.h"
 
 const int Weapon::getDamageMin() const {
-	return damageMin;
+	return damageMin; // return min damage
 }
 
 const int Weapon::getDamageMax() const {
-	return damageMax;
+	return damageMax; // return max damage
 }
 
 const int Weapon::getDamage() const {
-	return rand() % (damageMax - damageMin + 1) + (damageMin);
+	return rand() % (damageMax - damageMin + 1) + (damageMin); // return a random amount of damage
 }
 
 const int Weapon::getUses() const {
-	return usesLeft;
+	return usesLeft; // return the amount of uses
 }
 
 void Weapon::setUses(int uses) {
-	usesLeft = uses;
+	usesLeft = uses; // set uses
 }
 
 Weapon::~Weapon()
