06e9c2372b6b227f1eb36240590ff6a6b2da1af4
Connor Johnston <40429124@live.napier.ac.uk>
2021-04-20 04:40:46 +0100

    Update gameData.h

--- Sea_Chain/gameData.h
+++ Sea_Chain/gameData.h
@@ -3,15 +3,15 @@
 #include <string>
 #include "engine.h"
 
-class Singleton
+class Data
 {
 private:
-	static std::shared_ptr<Singleton> instance;
+	static std::shared_ptr<Data> instance;
 	static std::shared_ptr<Entity> player;
 
 public:
-	Singleton();
-	static std::shared_ptr<Singleton> getInstance();
+	Data();
+	static std::shared_ptr<Data> getInstance();
 	static std::shared_ptr<Entity> getPlayer();
 	static void setPlayer(std::shared_ptr<Entity> p);
 };
