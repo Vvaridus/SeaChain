6e750d8699f10c2e52c13c9bd2640a9a4503e5c5
Connor Johnston <40429124@live.napier.ac.uk>
2021-04-20 09:22:56 +0100

    Updated to C++17 & Added FilePath to gameData

--- Sea_Chain/gameData.cpp
+++ Sea_Chain/gameData.cpp
@@ -1,18 +1,36 @@
 #include "gameData.h"
 #include <string>
+#include <Windows.h>
+#include <iostream>
+#include <shlobj.h>
 
-std::shared_ptr<Entity> Data::player;
+
 std::shared_ptr<Data> Data::instance;
+std::shared_ptr<Entity> Data::player;
+std::filesystem::path Data::filePath;
+std::string Data::fileFolder;
 
 std::shared_ptr<Data> Data::getInstance() {
-	if (instance == nullptr)
+	if (instance == nullptr) {
 		instance = std::make_shared<Data>();
+		PWSTR path_tmp;
+		auto folder_path = SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, nullptr, &path_tmp);
+		filePath = path_tmp;
+		CoTaskMemFree(path_tmp);
+	}
 
 	return instance;
 }
 
 std::shared_ptr<Entity> Data::getPlayer() {
 	return player;
+}
+
+std::filesystem::path Data::getFilePath() {
+	return filePath;
+}
+std::string Data::getFileFolder() {
+	return fileFolder;
 }
 
 void Data::setPlayer(std::shared_ptr<Entity> p) {
--- Sea_Chain/gameData.h
+++ Sea_Chain/gameData.h
@@ -2,16 +2,22 @@
 #include <memory>
 #include <string>
 #include "engine.h"
+#include <filesystem>
+#include <fstream>
 
 class Data
 {
 private:
 	static std::shared_ptr<Data> instance;
 	static std::shared_ptr<Entity> player;
+	static std::filesystem::path filePath;
+	static std::string fileFolder;
 
 public:
 	Data();
 	static std::shared_ptr<Data> getInstance();
 	static std::shared_ptr<Entity> getPlayer();
+	static std::filesystem::path getFilePath();
+	static std::string getFileFolder();
 	static void setPlayer(std::shared_ptr<Entity> p);
 };
