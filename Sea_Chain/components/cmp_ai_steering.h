ea0aba939143acf920b183bf20c61b5a3d7e8790
Connor Johnston <40429124@live.napier.ac.uk>
2021-04-30 13:52:11 +0100

    Revert "assed AI base files"
    This reverts commit b0aec159b7d5253bb43fad220a7ca1a60d7723c4.

--- Sea_Chain/components/cmp_ai_steering.h
+++ Sea_Chain/components/cmp_ai_steering.h
@@ -2,47 +2,10 @@
 #include <ecm.h>
 #include "../helpers/steering.h"
 
-
-struct SteeringOutput
-{
-    sf::Vector2f direction;
-
-    float rotation;
-};
-
-class SteeringBehaviour {
-public:
-    virtual ~SteeringBehaviour() = default;
-
-    virtual SteeringOutput getSteering() const noexcept = 0;
-};
-
-class Seek : public SteeringBehaviour {
-private:
-    Entity* _character;
-    Entity* _target;
-    float _maxSpeed;
-public:
-    Seek() = delete;
-    Seek(Entity *character, Entity *target, float maxSpeed): _character(character), _target(target), _maxSpeed(maxSpeed){}
-    SteeringOutput getSteering() const noexcept;
-};
-
-class Chase : public SteeringBehaviour {
-private:
-    Entity* _character;
-    Entity* _target;
-    float _maxSpeed;
-public:
-    Chase() = delete;
-    Chase(Entity* character, Entity* target, float maxSpeed) : _character(character), _target(target), _maxSpeed(maxSpeed) {}
-    SteeringOutput getSteering() const noexcept;
-};
-
 class SteeringComponent : public Component {
 protected:
     Seek _seek;
-    Chase _chase;
+    Flee _flee;
     Entity* _player;
     bool validMove(const sf::Vector2f&) const;
 public:
